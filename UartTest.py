f194391aad9fc553c1a1e9b98f90f34a197d2861
Parthipan Siva <psiva7@gmail.com>
2021-09-21 17:43:17 -0400

    test code for uart radar config

---
+++ UartTest.py
@@ -0,0 +1,17 @@
+import serial
+from time import sleep
+
+ser = serial.Serial ("/dev/ttyS0", 115200)    #Open port with baud rate
+ser.write(('sensorStop\n').encode())
+sleep(0.03)
+read_buffer = ser.read(ser.in_waiting)
+sleep(0.03)
+print(read_buffer)
+'''
+while True:
+    received_data = ser.read()              #read serial port
+    sleep(0.03)
+    data_left = ser.inWaiting()             #check for remaining byte
+    received_data += ser.read(data_left)
+    print (received_data)                   #print received data
+'''
