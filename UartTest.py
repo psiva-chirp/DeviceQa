379acc517274768e2c550dfd5de5c50b48a62032
Parthipan Siva <psiva7@gmail.com>
2021-09-21 17:53:28 -0400

    remove un-used code and add manual test line

--- UartTest.py
+++ UartTest.py
@@ -11,18 +11,7 @@ for line in fid:
     print(read_buffer)
 
 '''
-ser.write(('sensorStop\n').encode())
-sleep(0.03)
-read_buffer = ser.read(ser.in_waiting)
-sleep(0.03)
-print(read_buffer)
-'''
+Manual test:
 
-'''
-while True:
-    received_data = ser.read()              #read serial port
-    sleep(0.03)
-    data_left = ser.inWaiting()             #check for remaining byte
-    received_data += ser.read(data_left)
-    print (received_data)                   #print received data
+python3 -m serial.tools.miniterm /dev/ttyS0 115200
 '''
