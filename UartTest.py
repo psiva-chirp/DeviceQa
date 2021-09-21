27406de6650f2d6b163a42a40b407ee7356f1e38
Parthipan Siva <psiva7@gmail.com>
2021-09-21 17:50:48 -0400

    send the config file

--- UartTest.py
+++ UartTest.py
@@ -2,11 +2,22 @@ import serial
 from time import sleep
 
 ser = serial.Serial ("/dev/ttyS0", 115200)    #Open port with baud rate
+fid = open('temp_radar_config.txt', 'r')
+for line in fid:
+    ser.write((line).encode())
+    sleep(0.03)
+    read_buffer = ser.read(ser.in_waiting)
+    sleep(0.03)
+    print(read_buffer)
+
+'''
 ser.write(('sensorStop\n').encode())
 sleep(0.03)
 read_buffer = ser.read(ser.in_waiting)
 sleep(0.03)
 print(read_buffer)
+'''
+
 '''
 while True:
     received_data = ser.read()              #read serial port
