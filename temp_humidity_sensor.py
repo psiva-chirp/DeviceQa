9462c321eaed1b3758b0118fe696b441f2d83a26
Parthipan Siva <psiva7@gmail.com>
2021-09-30 14:05:30 -0400

    make temp humidity class

--- temp_humidity_sensor.py
+++ temp_humidity_sensor.py
@@ -2,52 +2,73 @@
 from periphery import I2C
 import time
 
-def get_word(high, low):
-    return ((high & 0xFF) << 8) + (low & 0xFF)
+class SHTC3:
+    SHT_I2CADDR = 0x70
+    SHT_WAKEUP = 0x3517
+    SHT_SLEEP = 0xB098
+    SHT_NORMAL_TFIRST = 0x7866
+    SHT_PART_ID = 0xEFC8
 
-def temp_calc(temp_raw):
-    temp = -45+175*temp_raw/(2**16)
-    return temp
+    def __get_word(self, high, low):
+        return ((high & 0xFF) << 8) + (low & 0xFF)
 
-def humidity_calc(humidity_raw):
-    humidity = 100*humidity_raw/(2**16)
-    return humidity
+    def __temp_calc(self, temp_raw):
+        temp = -45+175*temp_raw/(2**16)
+        return temp
 
-device_address = 0x70
-wakeup = 0x3517
-sleep = 0xB098
-nomral_T_first_clk_stretch_dis = 0x7866
-part_id = 0xEFC8
-time_to_take_measurment_ms = 12.1
+    def __humidity_calc(self, humidity_raw):
+        humidity = 100*humidity_raw/(2**16)
+        return humidity
 
-i2c = I2C('/dev/i2c-0')
+    def __init__(self, i2c_bus='/dev/i2c-0'):
+        self.i2c_bus = i2c_bus
+        self.i2c = I2C('/dev/i2c-0')
+        self.time_to_take_measurment_ms = 12.1
+    
+    def get_part_id(self):
+        # read part id
+        query = [I2C.Message([self.SHT_PART_ID>>8, self.SHT_PART_ID&0xFF]), I2C.Message([0x00]*2, read=True)]
+        self.i2c.transfer(self.SHT_I2CADDR, query)
+        part_id = self.__get_word(query[1].data[0], query[1].data[1])
+        return part_id
+    
+    def get_temp_humidity(self):
 
-# read part id
-query = [I2C.Message([part_id>>8, part_id&0xFF]), I2C.Message([0x00]*2, read=True)]
-i2c.transfer(device_address, query)
-print(query[1].data)
+        #send wake up
+        self.i2c.transfer(self.SHT_I2CADDR, [I2C.Message([self.SHT_WAKEUP>>8, self.SHT_WAKEUP&0xFF])])
 
-#send wake up
-i2c.transfer(device_address, [I2C.Message([wakeup>>8, wakeup&0xFF])])
+        #send take measurment
+        self.i2c.transfer(self.SHT_I2CADDR, [I2C.Message([self.SHT_NORMAL_TFIRST>>8, self.SHT_NORMAL_TFIRST&0xFF])])
 
-#send take measurment
-i2c.transfer(device_address, [I2C.Message([nomral_T_first_clk_stretch_dis>>8, nomral_T_first_clk_stretch_dis&0xFF])])
+        #wait for measurmenet to complete. Note 1.25x is a buffer
+        time.sleep(self.time_to_take_measurment_ms*1.25/1000)
 
-#wait for measurmenet to complete. Note 1.25x is a buffer
-time.sleep(time_to_take_measurment_ms*1.25/1000)
+        # read the measurment
+        query = [I2C.Message([0x00]*6, read=True)]
+        self.i2c.transfer(self.SHT_I2CADDR, query)
 
-# read the measurment
-query = [I2C.Message([0x00]*6, read=True)]
-i2c.transfer(device_address, query)
+        temp_raw = self.__get_word(query[0].data[0], query[0].data[1])
+        temp_crc = query[0].data[2]
 
-temp_raw = get_word(query[0].data[0], query[0].data[1])
-temp_crc = query[0].data[2]
+        humidity_raw = self.__get_word(query[0].data[3], query[0].data[4])
+        humidity_crc = query[0].data[5]
 
-humidity_raw = get_word(query[0].data[3], query[0].data[4])
-humidity_crc = query[0].data[5]
+        # TO DO: check crc
 
-temp = temp_calc(temp_raw)
-humidity = humidity_calc(humidity_raw)
+        temp = self.__temp_calc(temp_raw)
+        humidity = self.__humidity_calc(humidity_raw)
 
-print(temp)
-print(humidity)
+        #send sleep
+        self.i2c.transfer(self.SHT_I2CADDR, [I2C.Message([self.SHT_SLEEP>>8, self.SHT_SLEEP&0xFF])])
+
+        return temp, humidity
+
+def main():
+    sensor = SHTC3()
+    print(sensor.get_part_id())
+    while True:
+        print(sensor.get_temp_humidity())
+        time.sleep(1)
+
+if __name__ == "__main__":
+    main()
